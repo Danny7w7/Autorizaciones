daf52e9877beb1412ad51eedf95513e2a18294d9
DannyZz <dannyzabaleta0@gmail.com>
2025-11-07 15:54:03 -0500

    Load database config from environment variables
    Replaced hardcoded database URI with values loaded from environment variables using python-dotenv. This improves security and flexibility by allowing configuration without code changes.

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://root:@localhost/autorizacion"
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{os.getenv("db_user")}:{os.getenv("db_pass")}@localhost/{os.getenv("db_name")}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
